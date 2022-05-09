a0382450af0ca928a4bf4b505c0efcc631e05921
toanbui1991 <toanbui1991@gmail.com>
2022-05-09 23:24:30 +0700

    update parse_vn_30_pe.py

--- parse_vn_30_pe.py
+++ parse_vn_30_pe.py
@@ -32,6 +32,8 @@ with Chrome(executable_path=driver_path, chrome_options=options) as driver:
     login_button = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'button#btnLoginAccount')))
     driver.execute_script("arguments[0].click();", login_button)
     time.sleep(5)
+
+    #parse  first page
     target_link = 'https://finance.vietstock.vn/doanh-nghiep-a-z/?page=1'
     driver.get(target_link)
     table_container = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div#az-container')))
@@ -48,7 +50,45 @@ with Chrome(executable_path=driver_path, chrome_options=options) as driver:
     table_html =soup.select_one('table.table.table-striped.table-bordered.table-hover.table-middle.pos-relative.m-b')
     tables = pd.read_html(str(table_html))
     table = tables[0]
+    print('the shape of table: {}'.format(table.shape))
     #find link for each stock code
-    links = soup.select()
-    print('table: ')
-    print(table)
+    #use beautifulsoup element methods like: get_text() and get('attr')
+    links = table_html.select('a.title-link')
+    links = [l.get('href') for l in links]
+    print('the length of links: {}'.format(len(links)))
+    table['link'] = links
+    table.to_csv('./data/stocks_page_1.csv')
+    
+    #continue to parse other pages
+    #prepare for the loop
+    page_number = 1
+    continue_flag = True
+    while continue_flag:
+        try:
+            #move to the next links
+            next_button = driver.find_element(By.CSS_SELECTOR, 'button#btn-page-next')
+            driver.execute_script("arguments[0].click();", next_button)
+            page_number += 1 #move the next page and increase page_number
+            time.sleep(5)
+            table_container = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div#az-container')))
+            page_source = driver.page_source
+            soup = BeautifulSoup(page_source, 'html.parser')
+            table_html =soup.select_one('table.table.table-striped.table-bordered.table-hover.table-middle.pos-relative.m-b')
+            tables = pd.read_html(str(table_html))
+            table = tables[0]
+            print('the shape of table: {}'.format(table.shape))
+            #find link for each stock code
+            #use beautifulsoup element methods like: get_text() and get('attr')
+            links = table_html.select('a.title-link')
+            links = [l.get('href') for l in links]
+            print('the length of links: {}'.format(len(links)))
+            table['link'] = links
+            table.to_csv('./data/stocks_page_{}.csv'.format(page_number))
+
+
+        except Exception as e:
+            print('Exception: {}'.format(e))
+            continue_flag = False
+
+#try to combine parsed data together
+# data = pd.read_csv('')
